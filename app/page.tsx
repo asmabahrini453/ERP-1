ca418da08d569aa6e270c5f167a3c85c8aac24e4
asma <bahriniasma453@gmail.com>
2025-02-14 13:42:32 +0100

    creating the Header component : Banner & NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import { Header } from "@/sections/Header";
 
 export default function Home() {
   return (
-    <div> hi</div>
+    <Header/>
   );
 }
---
+++ sections/Header.tsx
@@ -0,0 +1,39 @@
+import ArrowRight from "@/assets/icons/arrow-right.svg"
+import MenuIcon from "@/assets/icons/menu.svg"
+import Logo from "@/assets/images/logosaas.png"
+import Image from "next/image";
+
+export const Header = () => {
+    return (
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+        Optimisez votre flux de travail et augmentez votre productivité
+        </p>
+         <div className="inline-flex gap-1 items-center">
+             <p>Obtenez une démo gratuite</p>
+            <ArrowRight className="h-4 w-4 inline-flex justify-center items-center"/>
+         </div>
+      </div>
+    
+      <div className="py-5">
+        <div className="container">
+           <div className="flex items-center justify-between">
+              <Image src={Logo} alt="logo" height={40} width={40}/>
+              <MenuIcon className="h-5 w-5 md:hidden"/>
+              <nav className="hidden md:flex gap-6 items-center text-black/60 ">
+                <a href="#" > Acceuil</a>
+                <a href="#" > Services</a>
+                <a href="#"> Nouveautés</a>
+                <a href="#"  > Aide</a>
+                <a href="#" > Clients</a>
+                <button className="bg-black text-white font-medium inline-flex justify-center py-2 px-4 rounded-lg tracking-tight"> Get for free</button>
+            </nav>
+           </div>
+        </div>
+
+      </div>
+    </header>
+    );
+  };
+  
