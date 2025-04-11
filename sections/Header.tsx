6343c85f1714ae5faaa22ee98688dedb3552eaed
asma <bahriniasma453@gmail.com>
2025-04-11 18:34:50 +0200

    changing the header for the landing page

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,91 +1,142 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import ArrowRight from "@/assets/icons/arrow-right.svg";
 import MenuIcon from "@/assets/icons/menu.svg";
-
 import Logo from "@/assets/images/erplogo.png";
-import Image from "next/image";
 
-import { Sheet, SheetContent, SheetTrigger, SheetClose } from "@/components/ui/sheet";
+import Image from "next/image";
 import Link from "next/link";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 50;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled]);
+
   return (
-    <header>
-      {/* Banner */}
-      <div className="flex justify-center items-center py-1 bg-[#023E8A] text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">
-          Optimisez votre flux de travail et augmentez votre productivité
-        </p>
-        <div className="inline-flex gap-1 items-center">
-          <p>Obtenez une démo gratuite</p>
-          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
-        </div>
-      </div>
-
-      {/* Nav bar */}
-      <div className="py-1">
-        <div className="container">
-          <div className="flex items-center justify-between">
-           
-            <div className="flex justify-center items-center ">
-              <Image src={Logo} alt="logo" height={60} width={60}  />
-              <span className="font-bold tracking-tight text-[#383861] hidden lg:flex ">
-                ERP PRO
-              </span>
-            </div>
-
-           
-            <Sheet>
-              <SheetTrigger asChild>
-                <button className="md:hidden">
-                  <MenuIcon className="h-6 w-6" />
-                </button>
-              </SheetTrigger>
-              <SheetContent side="left" className="w-64 p-6 bg-white">
-                <div className="flex justify-between items-center">
-                  <span className="text-lg font-semibold">Menu</span>
-                  <SheetClose asChild>
-                   
-                  </SheetClose>
+    <>
+      <div className="fixed top-0 left-0 w-full z-50 flex justify-center">
+        <div  className={`transition-all duration-500 w-full ${
+            scrolled ? "max-w-3xl mx-auto mt-4 rounded-3xl shadow-lg" : ""
+          }`}>
+          <header className={`w-full ${scrolled ? "bg-white py-1.5 rounded-3xl" : "bg-white"}`}>
+            {/* Banner */}
+            {!scrolled && (
+              <div className="flex justify-center items-center py-1 bg-[#023E8A] text-white text-sm gap-3">
+                <p className="text-white/60 hidden md:block">
+                  Optimisez votre flux de travail et augmentez votre productivité
+                </p>
+                <div className="inline-flex gap-1 items-center">
+                  <p>Obtenez une démo gratuite</p>
+                  <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
                 </div>
-                <nav className="mt-6 flex flex-col gap-4 text-black/60">
-                  <SheetClose asChild>
-                    <a href="#" className="hover:text-black">Acceuil</a>
-                  </SheetClose>
-                  <SheetClose asChild>
-                    <a href="#" className="hover:text-black">Services</a>
-                  </SheetClose>
-                 
-                  <SheetClose asChild>
-                    <a href="#" className="hover:text-black">Tarification</a>
-                  </SheetClose>
-                  <SheetClose asChild>
-                    <a href="#" className="hover:text-black">Aide</a>
-                  </SheetClose>
-                
-                  <SheetClose asChild>
-                    <Link  href="/auth/sign-in" className="bg-[#023E8A] hover:bg-[#3BCEAB] text-white font-medium flex items-center justify-center py-2 px-4 rounded-lg tracking-tight">
+              </div>
+            )}
+
+            {/* Navbar */}
+            <div  className={`${scrolled ? "" : "py-4"}`}>
+              <div className="container mx-auto">
+                <div className="flex items-center justify-between  ">
+                  {/* Logo */}
+                  <div className="flex items-center gap-0">
+                    <Image src={Logo} alt="logo" 
+                     height={scrolled ? 36 : 60}
+                     width={scrolled ? 36 : 60}
+                     className="transition-all duration-300" />
+                     <span className="font-bold tracking-tight text-[#383861] hidden lg:flex ml-2">ERP PRO</span>
+                  </div>
+
+                  {/* Mobile Menu Icon */}
+                  <div className="md:hidden  z-50">
+                    <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                      <MenuIcon className="h-6 w-6 text-[#023E8A]" />
+                    </button>
+                  </div>
+
+                  {/*md devices */}
+                  <nav className="hidden md:flex gap-6 items-center text-black/60">
+                    <a href="#" className="hover:text-black">
+                      Acceuil
+                    </a>
+                    <a href="#" className="hover:text-black">
+                      Services
+                    </a>
+                    <a href="#" className="hover:text-black">
+                      Tarification
+                    </a>
+                    <a href="#" className="hover:text-black">
+                      Aide
+                    </a>
+                    <Link
+                      href="/auth/sign-in"
+                      className={`bg-[#023E8A] text-white font-medium rounded-lg tracking-tight hover:bg-[#3BCEAB] ${
+                        scrolled ? "py-1.5 px-3 text-sm" : "py-2 px-4"
+                      }`}  >
                       Se connecter
                     </Link>
-                  </SheetClose>
-                </nav>
-              </SheetContent>
-            </Sheet>
+                  </nav>
+                </div>
+              </div>
 
-          
-            <nav className="hidden md:flex gap-6 items-center text-black/60">
-              <a href="#" className="hover:text-black">Acceuil</a>
-              <a href="#" className="hover:text-black">Services</a>
-              <a href="#" className="hover:text-black">Tarification</a>
-              <a href="#" className="hover:text-black">Aide</a>
-              <Link  href="/auth/sign-in"  className="bg-[#023E8A] text-white font-medium py-2 px-4 rounded-lg tracking-tight hover:bg-[#3BCEAB]">
-                Se connecter
-              </Link>
-            </nav>
-          </div>
+              {/* Mobile Only Dropdown Menu */}
+              {isMenuOpen && (
+                <div className="md:hidden absolute top-full left-0 w-full bg-white shadow-md border-t border-gray-200 z-40">
+                  <div className="flex flex-col sm:flex-col sm:justify-center gap-4 px-6 py-4 text-black/70 text-center">
+                    <a
+                      href="#"
+                      className="hover:text-black"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Acceuil
+                    </a>
+                    <a
+                      href="#"
+                      className="hover:text-black"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Services
+                    </a>
+                    <a
+                      href="#"
+                      className="hover:text-black"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Tarification
+                    </a>
+                    <a
+                      href="#"
+                      className="hover:text-black"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Aide
+                    </a>
+                    <Link
+                      href="/auth/sign-in"
+                      className="bg-[#023E8A] hover:bg-[#3BCEAB] text-white font-medium py-2 px-4 rounded-lg tracking-tight mx-auto sm:mx-0"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Se connecter
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </div>
+          </header>
         </div>
       </div>
-    </header>
+    </>
   );
 };
