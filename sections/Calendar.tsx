7751ca08177230f306efce0932446022ec0818ab
asma <bahriniasma453@gmail.com>
2025-04-11 18:48:47 +0200

    changing the images position in features section for sm devices

--- sections/Calendar.tsx
+++ sections/Calendar.tsx
@@ -12,11 +12,10 @@ export const startingFeatures = [
     'Planifiez un événement avec un titre et une description.',
     'Invitez des participants pour une meilleure coordination.',
     'Définissez une date et une heure pour organiser votre emploi du temps.'
-  ];
-  
+];
 
-export  const Calendar: React.FC = () => (
-  <section className="sm:pl-28 xs:p-8 px-6  py-12 relative z-10">
+export const Calendar: React.FC = () => (
+  <section className="sm:pl-28 xs:p-8 px-6 py-12 relative z-10">
     <motion.div
       variants={staggerContainer(0.2, 0.5)} 
       initial="hidden"
@@ -24,37 +23,33 @@ export  const Calendar: React.FC = () => (
       viewport={{ once: false, amount: 0.25 }}
       className="2xl:max-w-[1280px] w-full mx-auto flex lg:flex-row flex-col gap-8"
     >
-     
-     <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)} 
-        className="flex-[0.75] flex justify-center flex-col"
-        >
-        <TypingText title="| la vue Calendrier" />
-        <TitleText title={<span>Planification et Suivi de votre Calendrier</span>} />
-
-        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px] ">
-            {startingFeatures.map((feature: string, index: number) => (
-            <Steps
-                key={feature}
-                number={`${index < 9 ? '0' : ''}${index + 1}`} 
-                text={feature}
-            />
-            ))}
-        </div>
-        </motion.div>
-
-
-      <div
-        className="flex-1 flex justify-center items-center"
-      >
+      {/* Image */}
+      <div className="flex-1 flex justify-center items-center order-1 sm:order-1">
         <Image
           src={taskflow}
           alt="get-started"
-          className="w-[90%] h-[90%] object-contain "
+          className="w-[90%] h-[90%] object-contain"
         />
       </div>
+
+      {/* Texte */}
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)} 
+        className="flex-[0.75] flex justify-center flex-col order-2 sm:order-2"
+      >
+        <TypingText title="| la vue Calendrier" />
+        <TitleText title={<span>Planification et Suivi de votre Calendrier</span>} />
+
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature: string, index: number) => (
+            <Steps
+              key={feature}
+              number={`${index < 9 ? '0' : ''}${index + 1}`} 
+              text={feature}
+            />
+          ))}
+        </div>
+      </motion.div>
     </motion.div>
   </section>
 );
-
-
--- sections/Taskflow.tsx
+++ sections/Taskflow.tsx
@@ -10,12 +10,12 @@ import taskflow from '@/assets/images/taskflow.png';
 
 export const startingFeatures = [
   'Ajoutez une nouvelle tâche avec un titre et une description.',
-  ' Attribuez-la à un membre de groupe si nécessaire.',
-  ' Spécifiez une date limite pour assurer le suivi.'
+  'Attribuez-la à un membre de groupe si nécessaire.',
+  'Spécifiez une date limite pour assurer le suivi.'
 ];
 
-export  const Taskflow: React.FC = () => (
-  <section className="sm:pl-28 xs:p-8 px-6  py-12 relative z-10">
+export const Taskflow: React.FC = () => (
+  <section className="sm:pl-28 xs:p-8 px-6 py-12 relative z-10">
     <motion.div
       variants={staggerContainer(0.2, 0.5)} 
       initial="hidden"
@@ -23,37 +23,33 @@ export  const Taskflow: React.FC = () => (
       viewport={{ once: false, amount: 0.25 }}
       className="2xl:max-w-[1280px] w-full mx-auto flex lg:flex-row flex-col gap-8"
     >
-     
-     <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)} 
-        className="flex-[0.75] flex justify-center flex-col"
-        >
-        <TypingText title="| la vue Taskflow" />
-        <TitleText title={<span>Gestion optimisée des tâches</span>} /> 
-
-        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
-            {startingFeatures.map((feature: string, index: number) => (
-            <Steps
-                key={feature}
-                number={`${index < 9 ? '0' : ''}${index + 1}`} 
-                text={feature}
-            />
-            ))}
-        </div>
-        </motion.div>
-
-
-      <div
-        className="flex-1 flex justify-center items-center"
-      >
+      {/* Image */}
+      <div className="flex-1 flex justify-center items-center order-1 sm:order-1">
         <Image
           src={taskflow}
           alt="get-started"
           className="w-[90%] h-[90%] object-contain"
         />
       </div>
+
+      {/* Texte */}
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)} 
+        className="flex-[0.75] flex justify-center flex-col order-2 sm:order-2"
+      >
+        <TypingText title="| la vue Taskflow" />
+        <TitleText title={<span>Gestion optimisée des tâches</span>} /> 
+
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature: string, index: number) => (
+            <Steps
+              key={feature}
+              number={`${index < 9 ? '0' : ''}${index + 1}`} 
+              text={feature}
+            />
+          ))}
+        </div>
+      </motion.div>
     </motion.div>
   </section>
 );
-
-
